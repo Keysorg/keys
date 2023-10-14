79bdddd5d49c983b03316c59bd693829d0bd0c3c
shifu-kobby <samuelarmahappiah@gmail.com>
2023-10-14 20:26:21 +0000

    update title

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = ({ children }) => {
   return (
     <div className='layout'>
       <Head>
-        <title>ecommerce store</title>
+        <title>Keys Entertainment</title>
       </Head>
       <header>
         <Navbar />
